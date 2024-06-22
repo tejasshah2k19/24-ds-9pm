30abaca4ffc9432b75fa90757d62d67fcaf329af
Tejas Shah <tejasshah2k19@gmail.com>
2024-06-22 21:57:09 +0530

    stack 3

--- stack.c
+++ stack.c
@@ -1,43 +1,77 @@
-#include<stdio.h>
-#define SIZE 5 
+#include <stdio.h>
+#include <stdlib.h>
 
-int stack[SIZE]; //STACK 
-int top=-1; 
+#define SIZE 5
 
+int stack[SIZE]; // STACK
+int top = -1;
 
-
-void push(int data){
-    top++;
-    stack[top] = data; 
-}
-
-void display(){
-    int i;
-    for(i=top;i>=0;i--){
-        printf("\n%d",stack[i]);
+void push(int data)
+{
+    if (top == SIZE - 1)
+    {
+        printf("\nSTACK FULL");
+    }
+    else
+    {
+        top++;
+        stack[top] = data;
     }
 }
 
-void pop(){
-
+void display()
+{
+    int i;
+    for (i = top; i >= 0; i--)
+    {
+        printf("\n%d", stack[i]);
+    }
 }
 
+void pop()
+{
+    if (top == -1)
+    {
+        printf("\nSTACK IS EMPTY\n");
+    }
+    else
+    {
+        printf("\n%d POPED", stack[top]);
+        top--;
+    }
+}
 
-int main(){
+int main()
+{
 
-    push(10);
-    push(20);
-    push(30);
+    int choice, num;
 
-    display(); //30 20 10 
+    while (-1)
+    {
+        printf("\n0 For EXIT");
+        printf("\n1 For PUSH\n2 For POP\n3 For Display\nEnter choice");
+        scanf("%d", &choice);
 
-    // push(40);
+        switch (choice)
+        {
+        case 0:
+            exit(0);
+        case 1:
+            printf("\nEnter number");
+            scanf("%d", &num);
+            push(num);
+            break;
+        case 2:
+            pop();
+            break;
+        case 3:
+            display();
+            break;
 
-    // pop();//40 
-    // pop();//30 
-
-    // display(); //20 10 
-
-
-    return 0; 
+        default:
+            printf("\nINVALID CHOICE PTA !!! \n");
+            break;
+        }
+    }
+    return 0;
 }
